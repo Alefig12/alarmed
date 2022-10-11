efe0184fed4b390450f35b126f3202ae1a18edd2
Marcela Hernandez <vmhernandez@uninorte.edu.co>
2022-10-11 17:39:10 -0500

    Co-authored-by: Sthephany Rojas <sthephanyr15@users.noreply.github.com> Co-authored-by: Jonathan Arias Rua <jony1754@hotmail.com>

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sort_child_properties_last, prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -14,13 +16,56 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: Key('LoginScaffold'),
-      body: SafeArea(
+        key: Key('LoginScaffold'),
+        body: SafeArea(
           child: Center(
-        child: Container(
-          color: Colors.blue,
-        ),
-      )),
-    );
+              child: Container(
+            // ignore: sort_child_properties_last
+            child: SizedBox(
+                width: 300.0,
+                height: 300.0,
+                child: Column(
+                  children: [
+                    Text('Alarmed'),
+                    Text('iniciar sesión'),
+                    Container(
+                        child: SizedBox(
+                          width: 250.0,
+                          height: 50.0,
+                          child: Column(children: [
+                            Row(children: [
+                              Text('Usuario:'),
+                              TextField(
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: 'Password',
+                                ),
+                              )
+                            ]),
+                            Row(children: [
+                              Text('Contraseña'),
+                              TextField(
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: 'Password',
+                                ),
+                              )
+                            ])
+                          ]),
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(16),
+                        ))
+                  ],
+                )),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 84, 84, 84),
+              borderRadius: BorderRadius.circular(25),
+            ),
+          )),
+        ));
   }
 }
