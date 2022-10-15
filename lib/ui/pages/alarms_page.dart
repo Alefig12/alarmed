421e2915a027c94293500866dd0cad51bae41eac
Jonathan Enrique Arias Rua <jony1754@hotmail.com>
2022-10-15 17:35:28 -0500

    cositas

--- lib/ui/pages/alarms_page.dart
+++ lib/ui/pages/alarms_page.dart
@@ -1,7 +1,13 @@
+// ignore_for_file: prefer_const_constructors
+// ignore: prefer_const_constructors
+
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:alarmed/ui/assets/constant.dart';
 import 'package:flutter/material.dart';
+import "dart:math" as math;
+import '../../custom_icons_icons.dart';
+import '../widgets/roundedbox_widget.dart';
 
 class AlarmPage extends StatefulWidget {
   @override
@@ -9,6 +15,7 @@ class AlarmPage extends StatefulWidget {
 }
 
 class _AlarmPageState extends State<AlarmPage> {
+  double _currentSliderValue = 0.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,15 +48,240 @@ class _AlarmPageState extends State<AlarmPage> {
                             color: Constant.secondCont,
                             borderRadius: BorderRadius.circular(15)),
                         child: Padding(
-                            padding: EdgeInsets.all(20.0),
-                            child: Row(
-                              // ignore: prefer_const_literals_to_create_immutables
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
                               children: [
-                                const Image(
-                                    image: AssetImage('images/pill.png')),
+                                Row(
+                                  // ignore: prefer_const_literals_to_create_immutables
+                                  children: [
+                                    Expanded(
+                                      flex: 1,
+                                      child: MainRoundedBox(
+                                        radius: 30,
+                                        width: 60,
+                                        height: 60,
+                                        color: Constant.inCont,
+                                        child: Center(
+                                          child: Transform.rotate(
+                                            angle: -math.pi / 1.65,
+                                            child: Icon(
+                                              CustomIcons.pill,
+                                              color: Colors.white,
+                                              size: 40,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    Expanded(
+                                      flex: 3,
+                                      child: Column(
+                                        // ignore: prefer_const_literals_to_create_immutables
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        // ignore: prefer_const_literals_to_create_immutables
+                                        children: [
+                                          // ignore: prefer_const_constructors
+                                          Text(
+                                            "Pastilla A",
+                                            // ignore: prefer_const_constructors
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          Text("Dia: x",
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                              )),
+                                          Text("Hora: xx:xx",
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                              )),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                // ignore: prefer_const_literals_to_create_immutables
+                                Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(
+                                        child: Center(
+                                          child: Text("Tono",
+                                              style: TextStyle(fontSize: 15)),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 2,
+                                        child: RoundTextInput(
+                                          color: Constant.mainCont,
+                                        ),
+                                      ),
+                                    ]),
+                                Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(
+                                        child: Center(
+                                          child: Text("Volumen",
+                                              style: TextStyle(fontSize: 15)),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 2,
+                                        child: Slider(
+                                          value: _currentSliderValue,
+                                          onChanged: (value) {
+                                            setState(() {
+                                              _currentSliderValue = value;
+                                            });
+                                          },
+                                          activeColor: Constant.mainCont,
+                                          inactiveColor: Constant.inCont,
+                                        ),
+                                      ),
+                                    ]),
+                                Align(
+                                  alignment: Alignment.centerRight,
+                                  child: RoundTextButton(
+                                      child: Text(
+                                        "GUARDAR",
+                                        style: TextStyle(
+                                            color: Constant.mainCont,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      color: Colors.white),
+                                )
                               ],
                             )),
                       ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Constant.secondCont,
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                              children: [
+                                Row(
+                                  // ignore: prefer_const_literals_to_create_immutables
+                                  children: [
+                                    Expanded(
+                                      flex: 1,
+                                      child: MainRoundedBox(
+                                        radius: 30,
+                                        width: 60,
+                                        height: 60,
+                                        color: Constant.inCont,
+                                        child: Center(
+                                          child: Transform.rotate(
+                                            angle: -math.pi / 1.65,
+                                            child: Icon(
+                                              CustomIcons.pill,
+                                              color: Colors.white,
+                                              size: 40,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    Expanded(
+                                      flex: 3,
+                                      child: Column(
+                                        // ignore: prefer_const_literals_to_create_immutables
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        // ignore: prefer_const_literals_to_create_immutables
+                                        children: [
+                                          // ignore: prefer_const_constructors
+                                          Text(
+                                            "Pastilla B",
+                                            // ignore: prefer_const_constructors
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          Text("Dia: x",
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                              )),
+                                          Text("Hora: xx:xx",
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                              )),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                // ignore: prefer_const_literals_to_create_immutables
+                                Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(
+                                        child: Center(
+                                          child: Text("Tono",
+                                              style: TextStyle(fontSize: 15)),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 2,
+                                        child: RoundTextInput(
+                                          color: Constant.mainCont,
+                                        ),
+                                      ),
+                                    ]),
+                                Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(
+                                        child: Center(
+                                          child: Text("Volumen",
+                                              style: TextStyle(fontSize: 15)),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 2,
+                                        child: Slider(
+                                          value: _currentSliderValue,
+                                          onChanged: (value) {
+                                            setState(() {
+                                              _currentSliderValue = value;
+                                            });
+                                          },
+                                          activeColor: Constant.mainCont,
+                                          inactiveColor: Constant.inCont,
+                                        ),
+                                      ),
+                                    ]),
+                                Align(
+                                  alignment: Alignment.centerRight,
+                                  child: RoundTextButton(
+                                      child: Text(
+                                        "GUARDAR",
+                                        style: TextStyle(
+                                            color: Constant.mainCont,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      color: Colors.white),
+                                )
+                              ],
+                            )),
+                      )
                     ]))));
   }
 }
