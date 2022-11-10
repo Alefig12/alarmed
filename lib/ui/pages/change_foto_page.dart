6342c999f9b2657321ae817eed18498e56977b23
Jonathan Enrique Arias Rua <jony1754@hotmail.com>
2022-11-10 15:07:06 -0500

    I just added the page, working on other shit but my pc is quemandose

---
+++ lib/ui/pages/change_foto_page.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+
+class ChangeFotoPage extends StatelessWidget {
+  const ChangeFotoPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+          children: [Text("Alarmed")],
+          mainAxisAlignment: MainAxisAlignment.center),
+    ));
+  }
+}
