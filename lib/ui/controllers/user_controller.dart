0412729c16658f9d07773474b74c58b296b28036
Alefig12 <62145703+Alefig12@users.noreply.github.com>
2022-11-10 00:48:51 -0500

    realtime database v2

--- lib/ui/controllers/user_controller.dart
+++ lib/ui/controllers/user_controller.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:alarmed/domain/entities/alarm.dart';
 import 'package:get/get.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:intl/intl.dart';
 
 class UserController extends GetxController {
   final _loggedUserId = "".obs;
@@ -29,15 +30,39 @@ class UserController extends GetxController {
   Future<void> addAlarmToLoggedUser(Alarm alarm) async {
     try {
       String json = jsonEncode(alarm.days);
+      String startDate =
+          DateFormat('yyyy-MM-dd – kk:mm').format(alarm.startDateTime);
+      String endDate =
+          DateFormat('yyyy-MM-dd – kk:mm').format(alarm.endDateTime);
 
-      await databaseRef
-          .child('userList')
+      var dbRef = FirebaseDatabase.instance
+          .ref()
+          .child("userList")
           .child(_loggedUserId.value)
-          .child('alarm')
-          .set({
-        'id': alarm.id,
-        'pillName': alarm.pillName
-      }); //Buscar como poner los dias, tambien buscar que se pone en el id, porque ahora solo genera 1 sola. Buscar como se recorreria las alarmas si las hago con el id.
+          .child('alarm');
+      var snapshot = await dbRef.get(); // 👈 Use await here
+      var a = snapshot.value as Map<dynamic, dynamic>;
+      a.forEach((key, values) {
+        print(values);
+      });
+
+      // await databaseRef
+      //     .child('userList')
+      //     .child(_loggedUserId.value)
+      //     .child('alarm')
+      //     .push()
+      //     .set({
+      //   'id': alarm.id,
+      //   'pillName': alarm.pillName,
+      //   'days': json,
+      //   'startDateTime': startDate,
+      //   'endDateTime': endDate,
+      //   'repeat': alarm.repeat,
+      //   'quantity': alarm.quantity,
+      //   'dose': alarm.dose,
+      //   'tone': alarm.tone,
+      //   'volume': alarm.volume
+      // }); // tambien buscar que se pone en el id, porque ahora solo genera 1 sola. Buscar como se recorreria las alarmas si las hago con el id.
     } catch (error) {
       print(error);
       return Future.error(error);
