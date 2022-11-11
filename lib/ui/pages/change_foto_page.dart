0d7adb7c1f7495e050ddd8f9d69770c138edd8e5
Jonathan Enrique Arias Rua <jony1754@hotmail.com>
2022-11-10 23:59:49 -0500

    god I hope i didnt break anything

--- lib/ui/pages/change_foto_page.dart
+++ lib/ui/pages/change_foto_page.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:alarmed/ui/widgets/roundedbox_widget.dart';
 import "package:flutter/material.dart";
+import 'package:alarmed/ui/assets/constant.dart';
+import 'dart:math' as math;
 
 class ChangeFotoPage extends StatelessWidget {
   const ChangeFotoPage({Key? key}) : super(key: key);
@@ -6,9 +11,95 @@ class ChangeFotoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Column(
-          children: [Text("Alarmed")],
-          mainAxisAlignment: MainAxisAlignment.center),
+      child: Expanded(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Alarmed",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Constant.title, fontSize: 40),
+            ),
+            Expanded(
+              child: Center(
+                child: MainRoundedBox(
+                  width: 320,
+                  height: 320,
+                  color: Constant.secondCont,
+                  child: Column(children: [
+                    Text(
+                      "Escoge tu foto de perfil",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    CircleAvatar(
+                      radius: 48,
+                      backgroundColor: Colors.brown.shade800,
+                      child: const Text('AH'),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        Column(children: [
+                          CircleAvatar(
+                            radius: 28,
+                            backgroundColor: Colors.blue,
+                            child: const Text('AH'),
+                          ),
+                          Text("Camara",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold))
+                        ]),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Column(children: [
+                          CircleAvatar(
+                            radius: 28,
+                            backgroundColor: Colors.blue,
+                            child: const Text('AH'),
+                          ),
+                          Text("Galeria",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold))
+                        ])
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                    )
+                  ]),
+                ),
+              ),
+            ),
+            RoundTextButton(
+              child: Text("Siguiente",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold)),
+              color: Constant.mainCont2,
+              width: 200,
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Omitir",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold)))
+          ],
+        ),
+      ),
     ));
   }
 }
